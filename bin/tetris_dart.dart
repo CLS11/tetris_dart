240e836b4f1a23f721facc141a03c79205249635
CLS11 <crystall.101600@gmail.com>
2024-11-13 21:27:09 +0530

    first commit

---
+++ bin/tetris_dart.dart
@@ -0,0 +1,5 @@
+import 'package:tetris_dart/tetris_dart.dart' as tetris_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tetris_dart.calculate()}!');
+}
---
+++ test/tetris_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:tetris_dart/tetris_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
