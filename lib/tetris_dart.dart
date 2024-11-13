511abd173f64f001baa039ab3ecc2b4d180aad0b
CLS11 <crystall.101600@gmail.com>
2024-11-14 00:10:43 +0530

    Imported dart core library.

--- lib/tetris_dart.dart
+++ lib/tetris_dart.dart
@@ -3,7 +3,9 @@ library tetris_dart;
 import 'dart:html';
 import 'dart:math';
 import 'dart:async';
+import 'dart:core';
 
 part 'src/model/tile.dart';
 part 'src/model/block.dart';
 part 'src/model/tetros.dart';
+part 'src/model/game.dart';
